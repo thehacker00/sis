418522257b418883edaf59a31ef7570b320227aa
thehacker00 <72139920+thehacker00@users.noreply.github.com>
2022-02-03 14:18:26 +0530

    Add files via upload

---
+++ includes/topbar.php
@@ -0,0 +1,28 @@
+  <nav class="navbar top-navbar bg-white box-shadow">
+            	<div class="container-fluid">
+                    <div class="row">
+                        <div class="navbar-header no-padding">
+                			<a class="navbar-brand" href="dashboard.php">
+                			    SRMS | Admin
+                			</a>
+                            
+                		</div>
+                        <!-- /.navbar-header -->
+
+                		
+                			<ul class="nav navbar-nav navbar-right" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                             
+                				
+                				    <li><a href="logout.php" class="color-danger text-center"><i class="fa fa-sign-out"></i> Logout</a></li>
+                					
+                		
+                            
+                			</ul>
+                            <!-- /.nav navbar-nav navbar-right -->
+                		</div>
+                		<!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.row -->
+            	</div>
+            	<!-- /.container-fluid -->
+            </nav>
